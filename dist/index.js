02b9b0b7afa9b8a2fc687ecddebe049d5c5c738a
Olawale Ibrahim <ibrahim.olawale13@gmail.com>
2023-01-05 19:05:49 +0100

    tuples

--- dist/index.js
+++ dist/index.js
@@ -6,4 +6,7 @@ let level;
 level = 1;
 level = "any";
 console.log(level);
+let numbers = [1, 2, 3];
+let numbers_new = [1, 2, 3, "hello world"];
+let user = [1, "Wale"];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ let sales: number = 123_456_789;
 let course: string = "TypeScript";
 let is_published: boolean = true;
 
+// any type 
 let level;
 level = 1;
 level = "any";
 console.log(level);
+
+// arrays
+let numbers: number[] = [1, 2, 3];
+let numbers_new: (number | string)[] = [1, 2, 3, "hello world"];
+
+// tuples (fixed length array)
+let user: [number, string] = [1, "Wale"]
